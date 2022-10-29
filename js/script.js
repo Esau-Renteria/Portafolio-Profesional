5afea0ddeea20e055130a46f986d2fbc9e5602cc
Esau <esaurenteria.isc@gmail.com>
2022-10-29 11:04:47 -0500

    Creacion de cambio de color

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ const toggleTheme = document.getElementById('toggle-theme');
 const toggleIcon = document.getElementById('toggle-icon');
 const toggleText = document.getElementById('toggle-text');
 
+const toggleColors = document.getElementById('toggle-colors');
+
+const rootStyles = document.documentElement.style;
+
 
 let menuVisible = false;
 //Funcion que oculta o muestra el menu
@@ -46,7 +50,7 @@ window.onscroll = function () {
 
 
 
-
+//Funcion del dark mode
 toggleTheme.addEventListener('click', ()=>{
     document.body.classList.toggle('dark')
     if(toggleIcon.src.includes('moon.svg')){
@@ -57,3 +61,11 @@ toggleTheme.addEventListener('click', ()=>{
         toggleText.textContent='DARK MODE'
     }
 });
+
+//Funcion cambio de color
+
+
+toggleColors.addEventListener("click", (e) =>{
+  rootStyles.setProperty("--primary-color", e.target.dataset.color);
+});
+
